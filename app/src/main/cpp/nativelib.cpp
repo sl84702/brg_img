5d23c63735d085483f94328462e96a2caa64a50a
Oleg Kulachenko <o.kulachenko@yadro.com>
2019-05-04 21:13:32 +0300

    Add context

--- app/src/main/cpp/nativelib.cpp
+++ app/src/main/cpp/nativelib.cpp
@@ -8,6 +8,7 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES2/gl2.h>
+#include <GLES/gl.h>
 
 
 #include <android/log.h>
@@ -51,6 +52,67 @@ Java_com_example_brg_1img_MainActivity_changeImage(
         LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
         return NULL;
     }
+
+    EGLConfig config;
+    EGLint numConfigs;
+
+    EGLint    contextAttribs[] =
+            {
+    #ifdef _OGLES3
+                    EGL_CONTEXT_CLIENT_VERSION, 3,
+    #else
+                    EGL_CONTEXT_CLIENT_VERSION, 2,
+    #endif
+                    EGL_NONE /* terminator */
+            };
+
+    EGLint    configAttribs[] =
+            {
+                    EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+                    EGL_RED_SIZE,        8,
+                    EGL_GREEN_SIZE,      8,
+                    EGL_BLUE_SIZE,       8,
+                    EGL_ALPHA_SIZE,      8,
+                    EGL_BUFFER_SIZE,     32,
+                    EGL_DEPTH_SIZE,      16,
+                    EGL_STENCIL_SIZE,    8,
+    #ifdef _OGLES3
+                    EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
+    #else
+                    EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+    #endif
+                    EGL_NONE /* terminator */
+            };
+
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    if (display == NULL) {
+        LOGE("error get display");
+    }
+
+    if ( !eglInitialize(display, NULL, NULL) ) {
+        LOGE("error Initialize display");
+    }
+
+    if( !eglChooseConfig(display, configAttribs, &config, 1, &numConfigs) ) {
+        LOGE("error choose config");
+    }
+
+
+    EGLSurface surface = eglCreatePbufferSurface(display, config, NULL);
+    if (surface == NULL) {
+        LOGE("error create pbuffer surface");
+    }
+
+
+    EGLContext context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+    if (surface == NULL) {
+        LOGE("error create context");
+    }
+
+    if ( !eglMakeCurrent(display, surface, surface, context) ) {
+        LOGE("error make current");
+    }
+
 
 
     return bitmap;
